4e5165c2769252c524c6e1f79c587c633a2b4106
Sahil Garg <sahilgarg8901@gmail.com>
2018-03-20 09:48:45 +0530

    app is working with internet only

--- inso_app/app/src/main/java/com/example/sahil/insi_app/MainActivity.kt
+++ inso_app/app/src/main/java/com/example/sahil/insi_app/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.sahil.insi_app
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.net.HttpURLConnection
 import java.net.URL
-import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    private fun updateBtnClicked(view: View){
-        val url = "https://baconipsum.com/api/?type=meat-and-filler"
+    protected fun updateBtnClicked(view: View){
+        val url = "https://insi.herokuapp.com/getStats"
         MyAsynTask().execute(url)
     }
 
@@ -33,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             //TOdo http call
             try {
                 val url = URL(p0[0])
-                val urlConnect = url.openConnection() as HttpsURLConnection
+                val urlConnect = url.openConnection() as HttpURLConnection/*HttpsURLConnection*/
                 urlConnect.connectTimeout= 7000
                 var inString= ConvertStreamTOstring(urlConnect.inputStream)
                 //cannot access to ui,
@@ -42,19 +43,27 @@ class MainActivity : AppCompatActivity() {
 
                 publishProgress(inString)
 
-            }catch (e:Exception){}
-            return ""
+            }catch (e:Exception){
+                Log.d("hello1", e.toString())
+            }
+            return " "
         }
-
+ 
         override fun onProgressUpdate(vararg values: String?) {
             //during task run
             try {
                 var json =JSONObject(values[0])
-                val nextInsideJson = json.getJSONObject("nextInsideJsonKey")
-                val finalValue = nextInsideJson.getString("finalKey")
+                /*val nextInsideJson = json.getJSONObject("nextInsideJsonKey")
+                val finalValue = nextInsideJson.getString("finalKey")*/
+                val p1= json.getJSONObject("temp_uptime")
+                val p2= p1.getJSONObject("2018-03-18")
+                val p3= p2.getJSONArray("total_obs")
+                val answer = p3.toString()
 
-                textView.text = finalValue
-            }catch (e:Exception){}
+                textView.text = answer
+            }catch (e:Exception){
+                Log.d("hello2", e.toString())
+            }
 
         }
 
@@ -76,7 +85,9 @@ class MainActivity : AppCompatActivity() {
 
             }while (line!=null)
             inputstream.close()
-        }catch (e:Exception){}
+        }catch (e:Exception){
+            Log.d("hello3", e.toString())
+        }
         return AllString
     }
 }
